d6cfe04dc5ef01f6ad8f91a69fc45042b60f2e7f
Lindsay Salisbury <lindsaysalisbury@Lindsays-Air.localdomain>
2023-03-23 15:35:17 -0600

    commit

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ app.use(express.json())
 
 app.use(express.static(`${__dirname}/public`))
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '1fbca6b36432429bb8de6c63176dac2a',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 const cities = ['Salt Lake City', 'Seattle', 'San Francisco']
 
 app.get('/api/cities', (req, res) => {
-    // rollbar.info('The cities were sent')
+    rollbar.info('The cities were sent')
     res.status(200).send(cities)
 })
 
@@ -23,17 +30,17 @@ app.post('/api/cities', (req, res) => {
    try {
        if (index === -1 && cityName !== '') {
            cities.push(cityName)
-           //rollbar.info(`New student name added: ${cityName}`)
+           rollbar.info(`New student name added: ${cityName}`)
            res.status(200).send(cities)
        } else if (cityName === ''){
-            // rollbar.warning('Empty input')
+            rollbar.warning('Empty input')
            res.status(400).send('You must enter a city.')
        } else {
-            // rollbar.warning('Input already exists')
+            rollbar.warning('Input already exists')
            res.status(400).send('That city already exists.')
        }
    } catch (err) {
-        // rollbar.error(err)
+        rollbar.error(err)
        console.log(err)
    }
 })
@@ -42,7 +49,7 @@ app.delete('/api/cities/:index', (req, res) => {
     const targetIndex = +req.params.index
     
     cities.splice(targetIndex, 1)
-    // rollbar.info(`City deleted`)
+    rollbar.info(`City deleted`)
     res.status(200).send(cities)
 })
 
